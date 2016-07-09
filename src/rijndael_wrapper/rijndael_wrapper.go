47f7529e3d984f59c75fb4b9793b9a5cf0db44d3
GUO Yixuan <culu.gyx@gmail.com>
2016-07-08 20:14:21 -0400

    namespace separate

--- src/rijndael_wrapper/rijndael_wrapper.go
+++ src/rijndael_wrapper/rijndael_wrapper.go
@@ -6,32 +6,32 @@ import (
 //const BlockSize = 32
 
 
-func NewCipher(key []byte) (Cipher_wrapper, error) {
+func NewCipher(key []byte) (Cipher, error) {
     var key32 [32]byte
     copy(key32[0:32], key)
     c := rijndael.NewCipher(&key32)
     //c_wrapper := new(Cipher_wrapper)
-    var c_wrapper Cipher_wrapper
+    var c_wrapper Cipher
     c_wrapper.c = *c
     return c_wrapper, nil
 }
 
-type Cipher_wrapper struct {
+type Cipher struct {
     c rijndael.Cipher
 }
 
-func (c Cipher_wrapper) BlockSize() int {
+func (c Cipher) BlockSize() int {
     return 32
 }
 
-func (c Cipher_wrapper) Decrypt(dst, src []byte) {
+func (c Cipher) Decrypt(dst, src []byte) {
     var src32, dst32 [32]byte
     copy(src32[:], src)
     c.c.Decrypt(&dst32, &src32)
     copy(dst[0:32], dst32[:])
 }
 
-func (c Cipher_wrapper) Encrypt(dst, src []byte) {
+func (c Cipher) Encrypt(dst, src []byte) {
     var src32, dst32 [32]byte
     copy(src32[:], src)
     c.c.Encrypt(&dst32, &src32)
