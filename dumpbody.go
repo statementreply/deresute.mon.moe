c2b4f62fc5a195e8f1744f6c3757a0c3d9fac471
GUO Yixuan <culu.gyx@gmail.com>
2016-07-11 17:39:25 -0400

    ft cbc first

--- dumpbody.go
+++ dumpbody.go
@@ -21,6 +21,7 @@ func main() {
 	} else {
 		body, _ = ioutil.ReadAll(os.Stdin)
 	}
+	fmt.Println(string(body))
 	resp_body := body
 	// remove extra tabs
 	resp_body = []byte(strings.Replace(string(resp_body), "\t", "", -1))
@@ -28,15 +29,16 @@ func main() {
 
 	reply := make([]byte, base64.StdEncoding.DecodedLen(len(resp_body)))
 	n, _ := base64.StdEncoding.Decode(reply, resp_body)
-	//print("written", n, "\n")
-	//print("replylen", len(reply), "\n")
+	print("written", n, "\n")
+	print("replylen", len(reply), "\n")
 	reply = reply[:n]
-	//fmt.Println("reply", reply)
+	fmt.Println("reply", hex.Dump(reply))
 
 	msg_iv = strings.Replace(msg_iv, "-", "", -1)
-	//fmt.Println("key", string(reply[len(reply)-32:]))
+	fmt.Println("msg_iv is ", msg_iv)
+	fmt.Println("key", string(reply[len(reply)-32:]))
 	plain2 := apiclient.Decrypt_cbc(reply[:len(reply)-32], []byte(msg_iv), reply[len(reply)-32:])
-	//fmt.Println("plain2", string(plain2))
+	fmt.Println("plain2", hex.Dump(plain2))
 
 	mp := make([]byte, base64.StdEncoding.DecodedLen(len(plain2)))
 	n, _ = base64.StdEncoding.Decode(mp, plain2)
