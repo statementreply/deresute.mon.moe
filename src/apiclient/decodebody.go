f52ee0405c3f5e760813a0962e177231cb0d55b6
GUO Yixuan <culu.gyx@gmail.com>
2016-07-12 05:40:25 -0400

    less comments

--- src/apiclient/decodebody.go
+++ src/apiclient/decodebody.go
@@ -17,10 +17,8 @@ func DecodeBody(body []byte, msg_iv string) interface{} {
 
 	reply := make([]byte, base64.StdEncoding.DecodedLen(len(resp_body)))
 	n, _ := base64.StdEncoding.Decode(reply, resp_body)
-	//print("written", n, "\n")
-	//print("replylen", len(reply), "\n")
+	// trim to n
 	reply = reply[:n]
-	//fmt.Println("reply", reply)
 
 	msg_iv = strings.Replace(msg_iv, "-", "", -1)
 	//fmt.Println("key", string(reply[len(reply)-32:]))
@@ -30,17 +28,11 @@ func DecodeBody(body []byte, msg_iv string) interface{} {
 		return content
 	}
 	plain2 := Decrypt_cbc(reply[:len(reply)-32], []byte(msg_iv), reply[len(reply)-32:])
-	//fmt.Println("plain2", string(plain2))
 
 	mp := make([]byte, base64.StdEncoding.DecodedLen(len(plain2)))
 	n, _ = base64.StdEncoding.Decode(mp, plain2)
 	mp = mp[:n]
-	//fmt.Print("mp is\n", hex.Dump(mp))
-	//var content map[string]interface{}
 	MsgpackDecode(mp, &content)
 
 	return content
-	//yy, _ := yaml.Marshal(content)
-	//fmt.Printf("%#v\n", content)
-	//fmt.Println(string(yy))
 }
