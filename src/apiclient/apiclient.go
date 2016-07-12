f9a5846baa3b76c49ff633ee2baeef77623cf4ce
GUO Yixuan <culu.gyx@gmail.com>
2016-07-12 06:15:16 -0400

    add jails

--- src/apiclient/apiclient.go
+++ src/apiclient/apiclient.go
@@ -41,6 +41,8 @@ type ApiClient struct {
 	VIEWER_ID_KEY []byte
 	SID_KEY       []byte
 	msg_iv        []byte
+	// holds plaintext temporarily
+	plain         string
 }
 
 func Lolfuscate(s string) string {
@@ -135,17 +137,20 @@ func NewApiClientFromConfig(configFile string) *ApiClient {
 
 func (client *ApiClient) Call(path string, args map[string]interface{}) map[string]interface{} {
 	// Prepare request body
-	// vid_iv is \d{32}
-	vid_iv_byte := make([]byte, 16)
-	n, err := crand.Read(vid_iv_byte)
-	if (n != 16) || (err != nil) {
-		log.Fatal("rand err", n, err)
+	var vid_iv string
+	{
+		// vid_iv is \d{32}
+		vid_iv_byte := make([]byte, 16)
+		n, err := crand.Read(vid_iv_byte)
+		if (n != 16) || (err != nil) {
+			log.Fatal("rand err", n, err)
+		}
+		var vid_iv_big big.Int
+		vid_iv_big.SetBytes(vid_iv_byte)
+		vid_iv_string := fmt.Sprintf("%032d", &vid_iv_big)
+		vid_iv = vid_iv_string[len(vid_iv_string)-32:]
+		//log.Fatal(vid_iv, " ", len(vid_iv))
 	}
-	var vid_iv_big big.Int
-	vid_iv_big.SetBytes(vid_iv_byte)
-	vid_iv_string := fmt.Sprintf("%032d", &vid_iv_big)
-	vid_iv := vid_iv_string[len(vid_iv_string)-32:]
-	//log.Fatal(vid_iv, " ", len(vid_iv))
 
 	args["viewer_id"] = vid_iv + base64.StdEncoding.EncodeToString(Encrypt_cbc([]byte(client.viewer_id_str), []byte(vid_iv), client.VIEWER_ID_KEY))
 
