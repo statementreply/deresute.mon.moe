0e799d95c8bc494832a94fcbb5dc72e69be293b9
GUO Yixuan <culu.gyx@gmail.com>
2016-07-18 20:27:27 -0400

    twitter interface

--- twitter.pl
+++ twitter.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/perl
+# usage: perl twitter.pl "status"  # utf8 encoded
 use common::sense;
 use Net::Twitter;
 use YAML qw(LoadFile);
+use Encode qw(decode encode);
+use utf8;
 
 my $config = LoadFile("secret.yaml");
 #print "$config\n";
@@ -13,8 +16,14 @@ my $nt = Net::Twitter->new(
     "access_token_secret" => $$config{"twitter_access_token_secret"},
 );
 
+my $status = "test11";
+
+if (@ARGV) {
+    $status = decode("UTF-8", pop(@ARGV));
+}
+
 eval {
-    my $result = $nt->update({ status => "test10"});
+    my $result = $nt->update({ status => $status });
     print "here", $result,"\n";
 };
 
