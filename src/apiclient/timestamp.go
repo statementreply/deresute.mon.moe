3c13e653a45f5049687a7ca594e338091a1751b3
GUO Yixuan <culu.gyx@gmail.com>
2016-07-19 16:38:33 -0400

    timestamp functions

---
+++ src/apiclient/timestamp.go
@@ -0,0 +1,22 @@
+package apiclient
+
+import (
+	"fmt"
+	"time"
+)
+
+func RoundTimestamp(in time.Time) time.Time {
+	s := in.Unix()
+	rounded := ((s-120)/900)*900 + 120
+	return time.Unix(rounded, 0)
+}
+
+func GetLocalTimestamp() string {
+	return TimeToTimestamp(time.Now())
+}
+
+func TimeToTimestamp(t time.Time) string {
+	itime := t.Unix()
+	timestamp := fmt.Sprintf("%d", itime)
+	return timestamp
+}
