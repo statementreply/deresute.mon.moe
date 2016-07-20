756f6ae7444c4b27528415460c4c6adf705873ce
GUO Yixuan <culu.gyx@gmail.com>
2016-07-19 23:26:30 -0400

    save status

--- periodic_twitter.pl
+++ periodic_twitter.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 use common::sense;
 
-my $cached_status = "";
+my $cache_filename = "cached_status";
+my $cached_status = read_file($cache_filename);
 
 while (1) {
     # -s: silent
@@ -14,7 +15,29 @@ while (1) {
         print "update status: $new_status\n";
         system qw(perl twitter.pl), $new_status;
         $cached_status = $new_status;
+        write_file($cache_filename, $cached_status);
     }
 } continue {
     sleep 120;
+}
+
+sub read_file {
+    my $filename = shift;
+    if (! -e $filename) {
+        return "";
+    }
+    open my $fh, "<", $filename;
+    my $n = read $fh, my $buf, 500;
+    if ($n == 500) {
+        print "full\n";
+    }
+    return $buf;
+}
+
+sub write_file {
+    my $filename = shift;
+    my $buf = shift;
+    open my $fh, ">", $filename;
+    print $fh $buf;
+    close $fh;
 }
