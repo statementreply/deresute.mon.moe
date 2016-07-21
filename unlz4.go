128a98eb8fe17f54fbb34b9b3ff61c5aa67c9284
GUO Yixuan <culu.gyx@gmail.com>
2016-07-21 00:23:11 -0400

    cmdline

--- unlz4.go
+++ unlz4.go
@@ -5,10 +5,14 @@ import (
 	"resource_mgr"
 	"io/ioutil"
 	"os"
+	"log"
 )
 
 func main() {
-	t := resource_mgr.Unlz4(os.Args[0])
+	if len(os.Args) < 3 {
+		log.Fatalln("./unlz4 infile outfile")
+	}
+	t := resource_mgr.Unlz4(os.Args[1])
 	fmt.Println(len(t))
-	ioutil.WriteFile(os.Args[1], t, 0644)
+	ioutil.WriteFile(os.Args[2], t, 0644)
 }
