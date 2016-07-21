ef81f5a137af115a99636f1cda75d61804c1cbbc
GUO Yixuan <culu.gyx@gmail.com>
2016-07-21 03:46:04 -0400

    data available after 2016-07

--- rankserver.go
+++ rankserver.go
@@ -730,8 +730,9 @@ func (r *RankServer) eventHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n", "event", "start", "end")
 	for _, e := range r.resourceMgr.EventList {
 		name := e.Name()
-		if e.Type() == 1 || e.Type() == 3 {
+		if (e.Type() == 1 || e.Type() == 3) && e.EventEnd().After(time.Unix(1467552720, 0)) {
 			// ranking information available
+
 			name = fmt.Sprintf(`<a href="qchart?event=%d">%s</a>`, e.Id(), name)
 		}
 		fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n", name, r.formatTime(e.EventStart()), r.formatTime(e.EventEnd()))
