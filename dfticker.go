ba296ee4aa78df952ffa4f836f8d84d237bfc5ba
GUO Yixuan <culu.gyx@gmail.com>
2016-07-23 04:58:33 -0400

    re-parameterize

--- dfticker.go
+++ dfticker.go
@@ -23,6 +23,40 @@ var lastRun = time.Unix(0, 0)
 var sleepDuration = time.Minute * 2
 
 func main() {
+
+	log.Println("local-timestamp", datafetcher.GetLocalTimestamp())
+	//client := apiclient.NewApiClientFromConfig(secret_file)
+	key_point := [][2]int{
+		[2]int{1, 1},
+		[2]int{1, 501},     // pt ranking emblem-1
+		[2]int{1, 2001},    // tier 1
+		[2]int{1, 5001},    // emblem-2
+		[2]int{1, 10001},   // tier 2
+		[2]int{1, 20001},   // tier 3
+		[2]int{1, 50001},   // tier 4-old
+		[2]int{1, 60001},   // tier 4
+		[2]int{1, 100001},  // tier 5-old
+		[2]int{1, 120001},  // tier 5
+		[2]int{1, 300001},  // tier 6
+		[2]int{1, 500001},  // tier 7
+		[2]int{1, 1000001}, // tier 8
+		[2]int{2, 1},       // score ranking top
+		[2]int{2, 5001},    // tier 1
+		[2]int{2, 10001},   // tier 2
+		[2]int{2, 40001},   // tier 3
+		[2]int{2, 50001},   // tier 4
+	}
+	// extra data points
+	for index := 0; index < 61; index++ {
+		key_point = append(key_point, [2]int{1, index*10000 + 1})
+		key_point = append(key_point, [2]int{2, index*10000 + 1})
+	}
+	client := apiclient.NewApiClientFromConfig(SECRET_FILE)
+	df := datafetcher.NewDataFetcher(client, key_point, RANK_CACHE_DIR)
+
+
+
+
 	ticker := time.NewTicker(time.Second * 1)
 	var q, q0 time.Duration
 	var r, mod time.Duration
@@ -30,7 +64,6 @@ func main() {
 	mod = time.Minute * 15
 	q0 = (time.Duration(time.Now().UnixNano()) - r) / mod
 
-	client := apiclient.NewApiClientFromConfig(SECRET_FILE)
 	for {
 		select {
 		case t := <-ticker.C:
@@ -39,22 +72,22 @@ func main() {
 			if (q > q0) || NeedToRun() {
 				fmt.Println("runCommand", t.String())
 				q0 = q
-				go runCommand(client, RANK_CACHE_DIR)
+				go runCommand(df)
 			}
 		}
 	}
 }
 
-func runCommand(client *apiclient.ApiClient, rank_cache_dir string) {
+func runCommand(df *datafetcher.DataFetcher) {
 	if !IsRunning() {
 		SetRunning()
-		err := df_main(client, rank_cache_dir)
+		err := df_main(df)
 		SetFinished()
 
 		if err != nil {
 			if err == apiclient.ErrSession {
 				// run again immediately
-				client.Reset_sid()
+				df.Client.Reset_sid()
 				lock.Lock()
 				lastRun = time.Unix(0, 0)
 				lock.Unlock()
@@ -99,36 +132,7 @@ func NeedToRun() bool {
 	return ret
 }
 
-func df_main(client *apiclient.ApiClient, rank_cache_dir string) error {
-	log.Println("local-timestamp", datafetcher.GetLocalTimestamp())
-	//client := apiclient.NewApiClientFromConfig(secret_file)
-	key_point := [][2]int{
-		[2]int{1, 1},
-		[2]int{1, 501},     // pt ranking emblem-1
-		[2]int{1, 2001},    // tier 1
-		[2]int{1, 5001},    // emblem-2
-		[2]int{1, 10001},   // tier 2
-		[2]int{1, 20001},   // tier 3
-		[2]int{1, 50001},   // tier 4-old
-		[2]int{1, 60001},   // tier 4
-		[2]int{1, 100001},  // tier 5-old
-		[2]int{1, 120001},  // tier 5
-		[2]int{1, 300001},  // tier 6
-		[2]int{1, 500001},  // tier 7
-		[2]int{1, 1000001}, // tier 8
-		[2]int{2, 1},       // score ranking top
-		[2]int{2, 5001},    // tier 1
-		[2]int{2, 10001},   // tier 2
-		[2]int{2, 40001},   // tier 3
-		[2]int{2, 50001},   // tier 4
-	}
-	// extra data points
-	for index := 0; index < 61; index++ {
-		key_point = append(key_point, [2]int{1, index*10000 + 1})
-		key_point = append(key_point, [2]int{2, index*10000 + 1})
-	}
-	df := datafetcher.NewDataFetcher(client, key_point, rank_cache_dir)
-
+func df_main(df *datafetcher.DataFetcher) error {
 	err := df.Run()
 	if err != nil {
 		log.Println(err)
