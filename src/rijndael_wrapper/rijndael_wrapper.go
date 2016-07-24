1482406a13b85749f6a96404810123a270d1c7a8
GUO Yixuan <culu.gyx@gmail.com>
2016-07-24 01:40:42 -0400

    use pointer receiver

--- src/rijndael_wrapper/rijndael_wrapper.go
+++ src/rijndael_wrapper/rijndael_wrapper.go
@@ -8,32 +8,32 @@ import (
 
 //const BlockSize = 32
 
-func NewCipher(key []byte) (Cipher, error) {
+func NewCipher(key []byte) (*Cipher, error) {
 	var key32 [32]byte
 	copy(key32[0:32], key)
 	c := rijndael.NewCipher(&key32)
 	//c_wrapper := new(Cipher_wrapper)
 	var c_wrapper Cipher
 	c_wrapper.c = *c
-	return c_wrapper, nil
+	return &c_wrapper, nil
 }
 
 type Cipher struct {
 	c rijndael.Cipher
 }
 
-func (c Cipher) BlockSize() int {
+func (c *Cipher) BlockSize() int {
 	return 32
 }
 
-func (c Cipher) Decrypt(dst, src []byte) {
+func (c *Cipher) Decrypt(dst, src []byte) {
 	var src32, dst32 [32]byte
 	copy(src32[:], src)
 	c.c.Decrypt(&dst32, &src32)
 	copy(dst[0:32], dst32[:])
 }
 
-func (c Cipher) Encrypt(dst, src []byte) {
+func (c *Cipher) Encrypt(dst, src []byte) {
 	var src32, dst32 [32]byte
 	copy(src32[:], src)
 	c.c.Encrypt(&dst32, &src32)
