8ff54682c8aa3a51b444d119787bacd26551f706
GUO Yixuan <culu.gyx@gmail.com>
2016-07-25 18:42:53 -0400

    twitter_ticker.go

---
+++ twitter_ticker.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"io/ioutil"
+	"net/http"
+	"bytes"
+	"os/exec"
+	"sync"
+	"regexp"
+)
+
+var wg sync.WaitGroup
+
+var updatingFilter = regexp.MustCompile("(^\\s*$)|UPDATING")
+
+type Periodic struct {
+	cache_filename string
+	url            string
+	interval       time.Duration
+	div, rem       time.Duration
+}
+
+
+func main() {
+	fmt.Println("go version of twitter ticker")
+	log.Println("go version of twitter ticker")
+	twitter1 := &Periodic{
+		cache_filename: "cached_status",
+		url:            "https://deresuteborder.mon.moe/twitter",
+		interval:       30 * time.Second,
+		div:            15*60 * time.Second,
+		rem:            2*60 * time.Second,
+	}
+	twitter2 := &Periodic{
+		cache_filename: "cached_status_emblem",
+		url:            "https://deresuteborder.mon.moe/twitter_emblem",
+		interval:       30*time.Second,
+		div:            15*60*time.Second,
+	    rem:            3*60*time.Second,
+	}
+	wg.Add(2)
+	go twitter1.Run()
+	go twitter2.Run()
+	// wait
+	wg.Wait()
+}
+
+func (p *Periodic) Run() {
+	defer wg.Done()
+	ticker := time.NewTicker(time.Second * 1)
+	//quotient := (time.Duration(time.Now().UnixNano()) - p.rem) / p.div;
+	quotient := time.Duration(0)
+
+	for {
+		select {
+		case t := <-ticker.C:
+			_ = t
+			quotient_new := (time.Duration(time.Now().UnixNano()) - p.rem) / p.div;
+			if (quotient_new <= quotient) {
+				continue
+			}
+			for {
+				content, err := ioutil.ReadFile(p.cache_filename)
+				if err != nil {
+					log.Println("cannot read cache_file", err)
+					content = []byte("")
+				}
+				//log.Println("content is", string(content))
+				var body, result []byte
+
+				resp, err := http.Get(p.url)
+				if err != nil {
+					log.Println("cannot get url", err)
+					goto Retry
+				}
+
+				body, err = ioutil.ReadAll(resp.Body)
+				resp.Body.Close()
+				if err != nil {
+					log.Println("cannot read respbody", err)
+					goto Retry
+				}
+
+				//log.Println("body is", string(body))
+				if bytes.Equal(body, content) {
+					goto Retry
+				}
+				if updatingFilter.Match(body) {
+					goto Retry
+				}
+
+
+				err = ioutil.WriteFile(p.cache_filename, body, 0644)
+				if err != nil {
+					log.Println("cannot write file", err)
+				}
+				result, err = exec.Command("perl", "twitter_fake.pl", string(body)).CombinedOutput()
+				log.Println(string(result))
+				if err != nil {
+					log.Println("error occured")
+				}
+
+				quotient = quotient_new
+				break
+			Retry:
+				time.Sleep(p.interval)
+			}
+		}
+	}
+}
