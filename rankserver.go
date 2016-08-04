fdf73624e574705644224cfe22ebf597d5f333bb
GUO Yixuan <culu.gyx@gmail.com>
2016-08-04 17:47:37 -0400

    remove script language attr

--- rankserver.go
+++ rankserver.go
@@ -637,8 +637,8 @@ func (r *RankServer) preload_html(w http.ResponseWriter, req *http.Request, para
 <title>デレステボーダーbotβ+</title>`, "\n")
 	fmt.Fprint(w, `<link rel="stylesheet" type="text/css" href="/static/style.css" />`, "\n")
 	fmt.Fprint(w, `<link rel="stylesheet" type="text/css" href="/static/jquery.mobile-1.4.5.min.css" />`, "\n")
-	fmt.Fprint(w, `<script language="javascript" type="text/javascript" src="/static/jquery-1.12.3.min.js"></script>`, "\n")
-	fmt.Fprint(w, `<script language="javascript" type="text/javascript" src="/static/jquery.mobile-1.4.5.min.js"></script>`, "\n")
+	fmt.Fprint(w, `<script type="text/javascript" src="/static/jquery-1.12.3.min.js"></script>`, "\n")
+	fmt.Fprint(w, `<script type="text/javascript" src="/static/jquery.mobile-1.4.5.min.js"></script>`, "\n")
 	//fmt.Fprintf(w, `<script language="javascript" type="text/javascript" src="%s"></script>`, r.generateDURL(param))
 
 	//if list_rank != nil {
