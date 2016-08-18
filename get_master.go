64f8f72dcccb9b9dacad1fd190e349be628d3a51
GUO Yixuan <culu.gyx@gmail.com>
2016-08-18 07:38:56 -0400

    query master.db by res_ver

---
+++ get_master.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"resource_mgr"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("res_ver needed")
+		return
+	}
+	res_ver := os.Args[1]
+	r := resource_mgr.NewResourceMgr(res_ver, "data/resourcesbeta")
+	fmt.Println("master is", r.LoadMaster())
+}
