e5656e692fb3a739f30757f92ebe948b15cd74de
GUO Yixuan <culu.gyx@gmail.com>
2016-08-18 16:44:05 -0400

    /event2 compared

--- src/rankserver/web.go
+++ src/rankserver/web.go
@@ -427,8 +427,9 @@ setMForm();
 func (r *RankServer) eventHandler(w http.ResponseWriter, req *http.Request) {
 	r.preload_html(w, req, nil)
 	defer r.postload_html(w, req)
-	fmt.Fprintf(w, `<table class="columns">`)
-	fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n", "event", "start", "second-half", "end")
+	fmt.Fprintf(w, `        <table class="columns">
+`)
+	fmt.Fprintf(w, "            <tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n", "event", "start", "second-half", "end")
 	formatter := ts.FormatTime
 	for _, e := range r.resourceMgr.EventList {
 		name := e.Name()
@@ -436,9 +437,9 @@ func (r *RankServer) eventHandler(w http.ResponseWriter, req *http.Request) {
 			// ranking information available
 			name = fmt.Sprintf(`<a href="qchart?event=%d">%s</a>`, e.Id(), name)
 		}
-		fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n", name, formatter(e.EventStart()), formatter(e.SecondHalfStart()), formatter(e.EventEnd()))
+		fmt.Fprintf(w, "            <tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n", name, formatter(e.EventStart()), formatter(e.SecondHalfStart()), formatter(e.EventEnd()))
 	}
-	fmt.Fprintf(w, `</table>`)
+	fmt.Fprintf(w, `        </table>`)
 }
 
 func (r *RankServer) logHandler(w http.ResponseWriter, req *http.Request) {
