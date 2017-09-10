ed33a6bdabcec643c5a12196da4b3a2bad34d509
GUO Yixuan <culu.gyx@gmail.com>
2017-09-09 23:38:43 -0400

    debug

--- res.go
+++ res.go
@@ -46,7 +46,7 @@ func main() {
 		//fmt.Println(e.NoticeStart(), e.EventStart(), e.Name())
 		//fmt.Println(utf8.RuneCountInString(e.Name()), e.Name())
 		//fmt.Println(e.ResultStart().Unix(), e.ResultEnd().Unix(), e.LongName())
-		fmt.Println(e.Type(), e.EventStart().String(), e.EventEnd().String(), e.LongName())
+		fmt.Println(e.Type(), e.EventStart().String(), e.EventEnd().String(), e.ResultEnd().String(), e.LongName())
 	}
 	currentEvent := resource_mgr.FindCurrentEvent(r.EventList)
 	if currentEvent != nil {
