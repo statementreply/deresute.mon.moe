cd32265df5332549ab8d1fb3848251455abf6486
GUO Yixuan <culu.gyx@gmail.com>
2018-01-16 04:50:20 -0500

    prepare for scout201801

--- scout201709.sh
+++ scout201709.sh
@@ -1,31 +1,48 @@
 set -x
-RES_VER=10030700
+RES_VER=10034100
 MASTER=$(./get_master $RES_VER)
 #echo $MASTER
+
 DB="file:$MASTER?mode=ro"
+
 # "SELECT * FROM gacha_data WHERE id == 30161;"
 #"/* number of cards */" \
 # "SELECT reward_id FROM gacha_available WHERE gacha_id == 30161;" 
 #"SELECT gacha_available.reward_id, card_data.rarity, card_data.name FROM gacha_available INNER JOIN card_data ON card_data.id == gacha_available.reward_id WHERE gacha_available.gacha_id == 30161 AND card_data.rarity > 6;"
 #"SELECT reward_id, card_data.name FROM gacha_available INNER JOIN card_data ON card_data.id = reward_id WHERE gacha_id = 30161 AND card_data.rarity > 6;"
 
-# 201709 scount ticket
+
+# GACHA_ID: id in gacha_data
+#    1xxxx: local audition gasha
+#    2xxxx: platinum ticket, 10-ticket, ssr-ticket, sr-ticket
+#    3xxxx: platinum audition gasha, (regular and limited)
+#    4xxxx: daily once 60
+#    7xxxx: ???
+
+
+# 201709 scout ticket (ssr and sr)
 GACHA_ID=30161
+
+# 201801 scout ticket
+GACHA_ID=30198
+
 
 # type
-GACHA_ID=30166 # cute
-GACHA_ID=30167 
-GACHA_ID=30168
-
-# for new sr scout
-GACHA_ID=30161
+#GACHA_ID=30166 # cute
+#GACHA_ID=30167 
+#GACHA_ID=30168
 
 
 # id, rarity, name
 sqlite3 "$DB" \
+ ".schema gacha_data" \
  "SELECT * FROM gacha_data WHERE id == $GACHA_ID;" \
  "SELECT count(*) FROM gacha_available WHERE gacha_id == $GACHA_ID;" \
  "/* all the card numbers */" \
  "SELECT count(*) FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity <= 4;" \
+ "SELECT count(*) FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity < 7 AND rarity >= 5;" \
  "SELECT id, rarity, name FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity < 7 AND rarity >= 5;" \
- "SELECT count(id) FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity < 9 AND rarity >= 7;"
+ "SELECT count(*) FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity < 9 AND rarity >= 7;" \
+ "SELECT id, rarity, name FROM gacha_available INNER JOIN card_data ON id = reward_id WHERE gacha_id = $GACHA_ID AND rarity < 9 AND rarity >= 7;"
+
+ #"SELECT id, source_guarantee, dicription FROM gacha_data ORDER BY id;" \
